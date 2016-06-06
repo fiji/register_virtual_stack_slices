409e17302cca93aae0c4c0fd4e83bb1403caa552
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2016-06-06 15:32:51 +0200

    Add test class for debugging

---
+++ src/test/java/register_virtual_stack_slices/RegisterTest.java
@@ -0,0 +1,19 @@
+package register_virtual_stack_slices;
+
+import ij.ImageJ;
+import register_virtual_stack.Register_Virtual_Stack_MT;
+
+public class RegisterTest {
+
+	/**
+	 * Main method to test and debug the Register Virtual Stack Slices GUI
+	 *
+	 * @param args
+	 */
+	public static void main( final String[] args )
+	{
+		ImageJ.main( args );
+
+		new Register_Virtual_Stack_MT().run( null );
+	}
+}
